3a204c73265ce59b7ed172e2601de6889b426191
Luis_P3 <Luis@PC-1>
2015-06-05 17:31:22 +0200

    Las !! del principio estan mal

--- gittest/src/gittest.cpp
+++ gittest/src/gittest.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello Mundo2!!!" << endl; // prints !!!Hello World!!!
+	cout << "Hello Mundo2!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
